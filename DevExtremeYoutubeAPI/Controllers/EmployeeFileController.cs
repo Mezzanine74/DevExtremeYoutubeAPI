4194c36fe9ee3e818d0a9892f518f0a85a584956
Savas Karaduman <savas.karaduman@keytours.com>
2022-07-30 17:45:59 +0300

    Finalizing DAL and api controllers

---
+++ DevExtremeYoutubeAPI/Controllers/EmployeeFileController.cs
@@ -0,0 +1,14 @@
+﻿using DevExtremeYoutubeDataAccess.DAL;
+using DevExtremeYoutubeModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevExtremeYoutubeAPI.Controllers
+{
+    public class EmployeeFileController : BaseApiController<EmployeeFileDal, EmployeeFile>
+    {
+    }
+}
